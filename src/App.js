ac9f46b745fbade9694d11893a084122b45b2fd8
Sanjana <44369175+ssanjanapai@users.noreply.github.com>
2020-08-20 09:53:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Search from './components/Search';
+import Recipes from './components/Recipes';
+import './App.css';
+
+class App extends React.Component {
+  
+  constructor(props)
+  {
+    super(props);
+    this.searchRecipe=this.searchRecipe.bind(this);
+    this.state={
+      recipes: []
+    }
+  }
+
+searchRecipe(recipename)
+{
+  fetch(`https://cors-anywhere.herokuapp.com/https://recipesapi.herokuapp.com/api/search?q=${recipename}`)
+  .then(response=>response.json())
+  //.then(data=>console.log("fectched json data",data))
+  .then(data=>this.setState({
+    recipes: data.recipes
+  }))
+  
+  // 
+}
+
+render()
+{
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h1>Recipe</h1>
+      </header>
+      <Search onSubmit={this.searchRecipe} />
+      <br />
+      <Recipes recipes={this.state.recipes}/>
+    </div>
+  );
+}
+}
+export default App;
